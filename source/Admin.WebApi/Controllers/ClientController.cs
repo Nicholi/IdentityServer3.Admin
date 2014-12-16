4a9a87208374996fbc9a4dd719bb243e3ba1782a
Christian Liebel <christian.liebel@thinktecture.com>
2014-12-16 11:19:54 +0100

    Prepared controllers for client and scope

---
+++ source/Admin.WebApi/Controllers/ClientController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Http;
+
+namespace Thinktecture.IdentityServer.v3.Admin.WebApi.Controllers
+{
+	public class ClientController : ApiController
+	{
+		 
+	}
+}
---
+++ source/Admin.WebApi/Controllers/ScopeController.cs
@@ -0,0 +1,7 @@
+﻿namespace Thinktecture.IdentityServer.v3.Admin.WebApi.Controllers
+{
+	public class ScopeController
+	{
+		 
+	}
+}
