3e1c41ae16cd110916510d06bde8c23091302e85
Christian Liebel <christian.liebel@thinktecture.com>
2014-12-16 12:00:03 +0100

    Dummy implementations for Client and Scope controllers and client store, introduced AutoMapper

--- source/Admin.WebApi/Controllers/ClientController.cs
+++ source/Admin.WebApi/Controllers/ClientController.cs
@@ -1,9 +1,54 @@
 ﻿using System.Web.Http;
+using Thinktecture.IdentityServer.v3.Admin.Storage;
+using Thinktecture.IdentityServer.v3.Admin.Storage.Models.Persistence;
 
 namespace Thinktecture.IdentityServer.v3.Admin.WebApi.Controllers
 {
 	public class ClientController : ApiController
 	{
-		 
+		private readonly ClientStore _clientStore;
+
+		public ClientController(ClientStore clientStore)
+		{
+			_clientStore = clientStore;
+		}
+
+		[HttpGet]
+		public IHttpActionResult List([FromUri] PagingInformation pagingInformation)
+		{
+			return Ok(_clientStore.List(pagingInformation));
+		}
+
+		[HttpGet]
+		public IHttpActionResult Get(int key)
+		{
+			return Ok(_clientStore.Get(key));
+		}
+
+		[HttpPost]
+		public IHttpActionResult Add()
+		{
+			// TODO: Add Web API Scope model as parameter and map it
+			_clientStore.Add(new object());
+
+			return Ok();
+		}
+
+		[HttpPut]
+		public IHttpActionResult Update()
+		{
+			// TODO: Add Web API Scope model as parameter and map it
+			_clientStore.Update(new object());
+
+			return Ok();
+		}
+
+		[HttpDelete]
+		public IHttpActionResult Delete(int key)
+		{
+			_clientStore.Delete(key);
+
+			return Ok();
+		}
 	}
 }
--- source/Admin.WebApi/Controllers/ScopeController.cs
+++ source/Admin.WebApi/Controllers/ScopeController.cs
@@ -1,9 +1,55 @@
 ﻿using System.Web.Http;
+using Thinktecture.IdentityServer.v3.Admin.Storage;
+using Thinktecture.IdentityServer.v3.Admin.Storage.Models.IdentityServer;
+using Thinktecture.IdentityServer.v3.Admin.Storage.Models.Persistence;
 
 namespace Thinktecture.IdentityServer.v3.Admin.WebApi.Controllers
 {
 	public class ScopeController : ApiController
 	{
-		 
+		private readonly ScopeStore _scopeStore;
+
+		public ScopeController(ScopeStore scopeStore)
+		{
+			_scopeStore = scopeStore;
+		}
+
+		[HttpGet]
+		public IHttpActionResult List([FromUri] PagingInformation pagingInformation)
+		{
+			return Ok(_scopeStore.List(pagingInformation));
+		}
+
+		[HttpGet]
+		public IHttpActionResult Get(int key)
+		{
+			return Ok(_scopeStore.Get(key));
+		}
+
+		[HttpPost]
+		public IHttpActionResult Add()
+		{
+			// TODO: Add Web API Scope model as parameter and map it
+			_scopeStore.Add(new Scope());
+
+			return Ok();
+		}
+
+		[HttpPut]
+		public IHttpActionResult Update()
+		{
+			// TODO: Add Web API Scope model as parameter and map it
+			_scopeStore.Update(new Scope());
+
+			return Ok();
+		}
+
+		[HttpDelete]
+		public IHttpActionResult Delete(int key)
+		{
+			_scopeStore.Delete(key);
+
+			return Ok();
+		}
 	}
 }
