8a8bef54e984b881f150438d324b0dde4710c6ff
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-01-26 21:09:30 +0100

    Added a dashboard controller.

---
+++ source/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Http;
+using Thinktecture.IdentityServer3.Admin.WebApi.Models.Dto;
+using Thinktecture.IdentityServer3.Admin.WebApi.Storage;
+
+namespace Thinktecture.IdentityServer3.Admin.WebApi.Controllers
+{
+    public class DashboardController : ApiController
+    {
+        private readonly ScopeStore _scopeStore;
+        private readonly ClientStore _clientStore;
+
+        public DashboardController(ScopeStore scopeStore, ClientStore clientStore)
+        {
+            _scopeStore = scopeStore;
+            _clientStore = clientStore;
+        }
+
+        [HttpGet]
+        public IHttpActionResult TotalCounts()
+        {
+            var result = new DashboardDto()
+            {
+                ClientStoreTotalCount = _clientStore.TotalCount(),
+                ScopeStoreTotalCount = _scopeStore.TotalCount()
+            };
+
+            return Ok(result);
+        }
+    }
+}
---
+++ source/Admin/Models/Dto/DashboardDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Thinktecture.IdentityServer3.Admin.WebApi.Models.Dto
+{
+    public class DashboardDto
+    {
+        public int ScopeStoreTotalCount { get; set; }
+        public int ClientStoreTotalCount { get; set; }
+    }
+}
