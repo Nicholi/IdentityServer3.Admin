9cc6c335d144625606b6bc8a9aa01e799e641ce7
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-03-07 14:53:13 +0100

    Example host can now prepopulate some test data

--- source/Host/Startup.cs
+++ source/Host/Startup.cs
@@ -1,7 +1,10 @@
-﻿using Admin.Host;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Admin.Host;
 using Microsoft.Owin;
 using Microsoft.Owin.Cors;
 using Owin;
+using Ploeh.AutoFixture;
 using Thinktecture.IdentityServer.Core.Configuration;
 using Thinktecture.IdentityServer3.Admin;
 using Thinktecture.IdentityServer3.Admin.Configuration;
@@ -18,37 +21,94 @@ namespace Admin.Host
     {
         private class InMemoryStorageRegistrations : StorageRegistrations
         {
+            private InMemoryScopeStorage _scopeStorage;
+            private InMemoryClientStorage _clientStorage;
+
+            public InMemoryStorageRegistrations(bool includeTestData)
+            {
+                CreateAndPopulateScopeStorage(includeTestData);
+                CreateAndPopulateClientStorage(includeTestData);
+            }
+
+            private void CreateAndPopulateClientStorage(bool includeTestData)
+            {
+                _clientStorage = new InMemoryClientStorage();
+
+                if (!includeTestData)
+                {
+                    return;
+                }
+
+                var fixture = new Fixture();
+                fixture.Behaviors.Clear();
+                fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+                var clients = fixture
+                    .Build<Client>()
+                    .WithAutoProperties()
+                    .Without(p => p.Id)
+                    .CreateMany(20);
+
+                foreach (var client in clients)
+                {
+                    _clientStorage.Add(client);
+                }
+            }
+
+            private void CreateAndPopulateScopeStorage(bool includeTestData)
+            {
+               _scopeStorage = new InMemoryScopeStorage();
+
+               if (!includeTestData)
+               {
+                   return;
+               }
+
+               var fixture = new Fixture();
+               fixture.Behaviors.Clear();
+               fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+                var scopes = fixture
+                    .Build<Scope>()
+                    .WithAutoProperties()
+                    .Without(p => p.Id)
+                    .CreateMany(20);
+
+                foreach (var scope in scopes)
+                {
+                    _scopeStorage.Add(scope);
+                }
+            }
+
             public override Registration<IPersistence<Scope>> ScopePersistence
             {
-                get { return new Registration<IPersistence<Scope>>(context => new InMemoryScopeStorage()); }
+                get { return new Registration<IPersistence<Scope>>(context => _scopeStorage); }
             }
 
             public override Registration<IPersistence<Client>> ClientPersistence
             {
-                get { return new Registration<IPersistence<Client>>(context => new InMemoryClientStorage()); }
+                get { return new Registration<IPersistence<Client>>(context => _clientStorage); }
             }
         }
 
         public void Configuration(IAppBuilder app)
         {
-            var options = CreateOptions();
+            var options = CreateOptions(true);
             app.UseIdentityServerAdmin(options);
         }
 
-        private IdentityServerAdminOptions CreateOptions()
+        private IdentityServerAdminOptions CreateOptions(bool includeTestData)
         {
             return new IdentityServerAdminOptions()
             {
-                StorageOptions = CreateStorageOptions(),
+                StorageOptions = CreateStorageOptions(includeTestData),
                 SecurityMode = new RemoteHostSecurityMode()
             };
         }
-        
-        private StorageOptions CreateStorageOptions()
+
+        private StorageOptions CreateStorageOptions(bool includeTestData)
         {
             return new StorageOptions()
             {
-                StorageRegistrations = new InMemoryStorageRegistrations()
+                StorageRegistrations = new InMemoryStorageRegistrations(includeTestData)
             };
         }
     }
